d3304fdd1d0bcf1cdd6a08b972f5aed93dc14e9a
Simen Støa <simen.stoa@bekk.no>
2020-01-08 09:31:42 +0100

    Added workout pace for vdot 57

--- frontend/src/Page/Profile.elm
+++ frontend/src/Page/Profile.elm
@@ -1,8 +1,7 @@
 module Page.Profile exposing (Model, Msg(..), Profile, Result, fetch, headerView, init, profileSelection, profileView, update, view)
 
-import Browser exposing (Document)
 import Config exposing (globalConfig)
-import Element exposing (Length, alignBottom, alignRight, centerX, centerY, fill, fillPortion, height, minimum, padding, px, spacing, text, width)
+import Element exposing (Length, alignRight, centerX, centerY, fill, fillPortion, height, minimum, padding, px, spacing, text, width)
 import Element.Background
 import Element.Border
 import Element.Font
@@ -16,6 +15,7 @@ import RemoteData exposing (RemoteData)
 import Treningsplan.Object
 import Treningsplan.Object.Profile
 import Treningsplan.Query
+import VDOT
 
 
 type Msg
@@ -112,16 +112,25 @@ view model =
 
 profileView : Profile -> Element.Element Msg
 profileView profile =
+    let
+        maybeVdot =
+            VDOT.getVdot (profile.vdot |> String.fromInt)
+    in
     Element.column [ height fill, width fill, padding 40, spacing 40 ]
         [ Element.row
             [ width fill, height <| fillPortion 1, spacing 40 ]
-            [ Element.el [ width <| fillPortion 2, height fill, Element.Background.color <| Pallette.light_moss_green ] <|
-                Element.el [ centerY, centerX, heading 1, Fonts.heading, Element.Font.size 30, padding 20 ] <|
+            [ Element.el
+                [ width <| (fillPortion 2 |> minimum 265)
+                , height fill
+                , Element.Background.color <| Pallette.light_moss_green
+                ]
+              <|
+                Element.el [ centerY, heading 1, Fonts.heading, Element.Font.size 30, padding 20 ] <|
                     text <|
                         profile.firstname
                             ++ " "
                             ++ profile.surname
-            , Element.el [ width <| fillPortion 3 ] <|
+            , Element.el [ height <| fillPortion 1, width <| fillPortion 3, Element.inFront <| vdotView profile ] <|
                 Element.none
             ]
         , Element.wrappedRow
@@ -130,16 +139,34 @@ profileView profile =
                 smallHeader <|
                     "Personal records"
             , Element.column [ height (fill |> minimum 400), width <| (fillPortion 3 |> minimum 300) ]
-                [ Element.el
+                [ Element.column
                     [ heading 2
                     , height fill
                     , width fill
                     , Element.Background.color <| Pallette.light_slate_grey
-                    , Element.inFront <| vdotView profile
                     ]
                   <|
-                    smallHeader <|
+                    [ smallHeader <|
                         "Workout pace"
+                    , case maybeVdot of
+                        Just vdot ->
+                            Element.column [ padding 20, spacing 20 ] <|
+                                Element.row [ Element.Font.bold, spacing 30, width (fill |> minimum 130) ]
+                                    [ Element.el [ width fill ] <| Element.text "Intensity"
+                                    , Element.text "Pace (min/km)"
+                                    ]
+                                    :: List.map
+                                        (\workoutPace ->
+                                            Element.row [ spacing 30, width (fill |> minimum 100) ]
+                                                [ Element.el [ width (fill |> minimum 130) ] <| Element.text <| VDOT.intensityToString workoutPace.intensity
+                                                , Element.text <| VDOT.workoutPaceToString workoutPace.pace
+                                                ]
+                                        )
+                                        vdot.workoutPace
+
+                        Nothing ->
+                            Element.text "VDOT data must be added before you can view workout paces."
+                    ]
                 ]
             ]
         ]
@@ -155,13 +182,16 @@ smallHeader header =
 vdotView : Profile -> Element.Element Msg
 vdotView profile =
     Element.el
-        [ height <| px 100
-        , width <| px 100
+        [ height <| px 95
+        , width <| px 95
         , alignRight
-        , Element.moveUp 25
-        , Element.moveRight 25
+        , Element.moveUp 10
+        , Element.moveRight 5
         , Element.Background.color <| Pallette.light_moss_green
         , Element.Border.rounded 150
+        , Element.Border.solid
+        , Element.Border.width 3
+        , Element.Border.color Pallette.sunray
         ]
     <|
         Element.el [ centerX, centerY ] <|
---
+++ frontend/src/VDOT.elm
@@ -0,0 +1,108 @@
+module VDOT exposing (getVdot, intensityToString, workoutPaceToString)
+
+import List.Extra
+
+
+type alias VDOT =
+    { value : String
+    , racePrediction : List RacePrediction
+    , workoutPace : List WorkoutPace
+    }
+
+
+type Pace
+    = SecPerKm Int
+
+
+type PaceDescription
+    = Constant Pace
+    | Between Pace Pace
+
+
+minPerKm : Pace -> String
+minPerKm pace =
+    let
+        minutes =
+            case pace of
+                SecPerKm secPerKm ->
+                    floor (toFloat secPerKm / 60)
+
+        seconds =
+            case pace of
+                SecPerKm secPerKm ->
+                    modBy 60 secPerKm
+    in
+    String.fromInt minutes ++ ":" ++ (String.padLeft 2 '0' <| String.fromInt seconds)
+
+
+workoutPaceToString : PaceDescription -> String
+workoutPaceToString paceDescription =
+    case paceDescription of
+        Constant pace ->
+            minPerKm pace
+
+        Between minPace maxPace ->
+            minPerKm minPace ++ "-" ++ minPerKm maxPace
+
+
+type Intensity
+    = Easy
+    | Interval
+    | Threshold
+    | Marathon
+    | Repitition
+
+
+intensityToString : Intensity -> String
+intensityToString intensity =
+    case intensity of
+        Easy ->
+            "Easy"
+
+        Interval ->
+            "Interval"
+
+        Threshold ->
+            "Threshold"
+
+        Marathon ->
+            "Marathon"
+
+        Repitition ->
+            "Repitition"
+
+
+type alias WorkoutPace =
+    { intensity : Intensity, pace : PaceDescription }
+
+
+type alias RacePrediction =
+    { race : Race, pace : Pace }
+
+
+type Distance
+    = Meter Int
+
+
+type alias Race =
+    { name : String, distance : Distance }
+
+
+table : List VDOT
+table =
+    [ { value = "57"
+      , racePrediction = [ { race = { name = "10k", distance = Meter 10000 }, pace = SecPerKm 222 } ]
+      , workoutPace =
+            [ { intensity = Easy, pace = Between (SecPerKm 276) (SecPerKm 305) }
+            , { intensity = Marathon, pace = Constant (SecPerKm 243) }
+            , { intensity = Threshold, pace = Constant (SecPerKm 230) }
+            , { intensity = Interval, pace = Constant (SecPerKm 211) }
+            , { intensity = Repitition, pace = Constant (SecPerKm 196) }
+            ]
+      }
+    ]
+
+
+getVdot : String -> Maybe VDOT
+getVdot vdot =
+    List.Extra.find (\v -> v.value == vdot) table
